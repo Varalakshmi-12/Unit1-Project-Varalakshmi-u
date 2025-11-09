68092d262989a44b3f25db6b47aa5499e458939b
Varalakshmi12 <pandu.chetan27@gmail.com>
2025-11-09 15:06:55 -0600

    changes in Contact.jsx commited

--- storeemployeetoolkit/src/pages/Contact.jsx
+++ storeemployeetoolkit/src/pages/Contact.jsx
@@ -1,5 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
 export default function Contact() {
   return (
-    <h1>Contact us</h1>
+    <section>
+    <div>
+    <h1>Contact Info</h1>
+    <div>
+    <p><strong>Support Team:</strong> +1 800 123 4567</p>
+    <p><strong>Office Number:</strong> +1 654 123 4567</p>
+    <p><strong>Manager Number:</strong> +1 654 658 4567</p>
+    <p><strong>Office Email:</strong> support@stores.com</p>
+    </div>
+    </div>
+    <nav className="nav-home">
+      <Link to="/">Go Back to Home</Link>
+      </nav>
+      </section>
   );
 }
