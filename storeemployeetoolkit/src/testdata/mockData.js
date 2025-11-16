f0aa50ad121022902335be34f6a8201b3d00360b
Varalakshmi12 <pandu.chetan27@gmail.com>
2025-11-16 12:38:01 -0600

    mockdata for cartpage is commited.

---
+++ storeemployeetoolkit/src/testdata/mockData.js
@@ -0,0 +1,7 @@
+export const mockItems = [
+  { id: 1, name: "Apple", price: 2 },
+  { id: 2, name: "Banana", price: 1 },
+  { id: 3, name: "Milk", price: 4 },
+  { id: 4, name: "Bread", price: 3 },
+  { id: 5, name: "Rice", price: 10 },
+];
